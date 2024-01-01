78c22182716420b90f87f455a9ccea0941f4ab64
gattumadhu <66872439+gattumadhu@users.noreply.github.com>
2024-01-01 11:53:48 +0530

    added normal xml file as well

--- src/main/java/com/weva/common/ExtentTestReports.java
+++ src/main/java/com/weva/common/ExtentTestReports.java
@@ -7,7 +7,6 @@ import org.Weva.constants.Constants;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentTestReports 
 {
